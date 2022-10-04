e87dbff0a84e2cf8558990aa1b160ed3c758097a
Selina <105521221+sesu0722@users.noreply.github.com>
2022-10-04 17:56:07 -0400

    update chart.js

--- static/js/chart.js
+++ static/js/chart.js
@@ -1,19 +1,14 @@
     
     getData();
     async function getData() {
-       const response = await fetch("/gun_violenceDB/combine2019");
+       const response = await fetch("/gun_violenceDB/combinData_byState");
        const data = await response.json();
        console.log(data);
-       
        
        var dataset=data[0]
        console.log(dataset)
 
-       //length = dataset.State.length;
-       //console.log(length);
-       
 
-       // Bar chart
        labels = [];
        values = [];
        values_a =[];
@@ -27,6 +22,9 @@
           values_m.push(dataset.massShootings[i])
           values_f.push(dataset.fatalPoliceShootings[i])
        }
+        
+       console.log(labels)
+       // Bar chart
        new Chart(document.getElementById("bar-chart"), {
           type: 'horizontalBar',
           data: {
@@ -44,9 +42,19 @@
              title: {
                 display: true,
                 text: 'U.S Death Count'
-             }
+             },
+             scales: {
+               y:[{
+                  ticks:{
+                     display: true,
+                     autoSkip: false
+                  }
+               }],
+               
+           }
           }
        });
+       // line chart
        new Chart(document.getElementById("line-chart"), {
         type: 'line',
         data: {
@@ -80,9 +88,114 @@
            }
         }
      });
+
+
    
     
     }
- 
+   //Pie chart
+   getData2();
+    async function getData2() {
+       const response = await fetch("/gun_violenceDB/combineData_StateParty");
+       const data = await response.json();
+       console.log(data);
+       
+       var dataset_p=data[0]
+       console.log(dataset_p)
 
-// line chart
+      
+       values_a = [dataset_p.accidentalDeath.Democratic, dataset_p.accidentalDeath.Republican];
+       values_m = [dataset_p.massShootings.Democratic, dataset_p.massShootings.Republican];
+       values_f = [dataset_p.fatalPoliceShootings.Democratic, dataset_p.fatalPoliceShootings.Republican];
+      
+       console.log(values_a)
+      
+       new Chart(document.getElementById("pie-chart"), {
+          type: 'pie',
+          data: {
+             labels: ["Democratic","Republican"],
+             datasets: [
+                {
+                   label: "Death by Party Affiliations",
+                   backgroundColor: ['blue','red'],
+                   data: values_a
+                }
+             ]
+          },
+          options: {
+            responsive: true,
+            plugins: {
+               legend: {display: true, position: 'right'},
+               title: {
+                  display: true,
+                  text: 'AccidentalDeath by Party',
+                  padding:{
+                     top: 10,
+                     bottom:20
+                  }
+               }
+             }
+           },
+              
+           });
+
+         new Chart(document.getElementById("pie-chart2"), {
+            type: 'pie',
+            data: {
+               labels: ["Democratic","Republican"],
+               datasets: [
+                  {
+                     label: "Death by Party Affiliations",
+                     backgroundColor: ['blue','red'],
+                     data: values_m
+                  }
+               ]
+            },
+            options: {
+              responsive: true,
+              plugins: {
+                 legend: {display: true,},
+                 title: {
+                    display: true,
+                    text: ' MassShooting Death by Party',
+                    padding:{
+                       top: 10,
+                       bottom:20
+                    }
+                 }
+               }
+             },
+                
+             });
+         new Chart(document.getElementById("pie-chart3"), {
+            type: 'pie',
+            data: {
+               labels: ["Democratic","Republican"],
+               datasets: [
+                  {
+                     label: "Death by Party Affiliations",
+                     backgroundColor: ['blue','red'],
+                     data: values_f
+                  }
+               ]
+            },
+            options: {
+               responsive: true,
+               plugins: {
+                  legend: {display: true,},
+                  title: {
+                     display: true,
+                     text: 'fatalPoliceShooting by Party',
+                     padding:{
+                        top: 10,
+                        bottom:20
+                     }
+                  }
+               }
+               },
+                  
+               });
+          
+    
+      }
+
